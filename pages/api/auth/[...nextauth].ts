11f524601f5cb9eae4e5127cc2b03844ed4bf859
Gus Ryan <gus.ryan163@gmail.com>
2023-03-21 15:03:35 -0700

    - add qbo api types - add functionality for storing access code and realmid in session

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,12 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
+import NextAuth from "next-auth"
 
-import { OAuthConfig } from "next-auth/providers"
+import type { Account, NextAuthOptions } from "next-auth"
+import type { JWT } from "next-auth/jwt"
+import type { OAuthConfig } from "next-auth/providers"
 
-const customProvider: OAuthConfig<any> = {
+import type { QBOProfile } from "../../../lib/util"
+
+const customProvider: OAuthConfig<QBOProfile> = {
   id: "QBO",
   name: "QBO",
   clientId: process.env.CLIENT_ID,
@@ -11,16 +15,50 @@ const customProvider: OAuthConfig<any> = {
   version: "2.0",
   wellKnown: process.env.WELL_KNOWN,
   authorization: {
-    params: { scope: "openid" },
+    params: { scope: "com.intuit.quickbooks.accounting openid profile address email phone" },
   },
   idToken: true,
   checks: ["pkce", "state"],
-  profile(profile) {
-    console.log("profile: ", profile)
-    return {
-      id: profile.sub,
-    }
-  },
+  profile: profile => ({
+    id: profile.sub,
+  }),
+}
+
+const jwtCallback: any = async ({
+  token,
+  account,
+  profile,
+}: {
+  token: JWT
+  account?: Account | null
+  profile?: QBOProfile | null
+}): Promise<JWT> => {
+  const qboProfile = profile as QBOProfile
+  if (account) {
+    console.log(account)
+    token.accessToken = account.access_token
+    token.id = account.providerAccountId
+
+    const qboProfile = await (
+      await fetch("https://sandbox-accounts.platform.intuit.com/v1/openid_connect/userinfo", {
+        headers: {
+          Authorization: `Bearer ${token.accessToken}`,
+          Accept: "application/json",
+        },
+      })
+    ).json()
+
+    token.email = qboProfile.email || "No email associated with account"
+    token.name = qboProfile.givenName || "No name associated with account"
+    // TODO fetch profile image URL
+    token.image = ""
+  }
+
+  if (profile) {
+    token.realmId = qboProfile.realmid as string
+  }
+
+  return token
 }
 
 export const authOptions: NextAuthOptions = {
@@ -32,39 +70,21 @@ export const authOptions: NextAuthOptions = {
     secret: process.env.JWT_SECRET,
   },
   callbacks: {
-    async session({ session, token, user }) {
-      const newSession: any = session
-      newSession.accessToken = token.accessToken
-      newSession.user.id = token.id
-      return newSession
+    async session({ session, token }) {
+      return {
+        ...session,
+        accessToken: token.accessToken,
+        realmId: token.realmId,
+        user: {
+          id: token.id,
+          name: token.name,
+          image: token.image,
+          email: token.email,
+        },
+      } as any
     },
-    async jwt({ token, user, account, profile, isNewUser }) {
-      console.log("jwt callback")
-      console.log("isNewUser: ", isNewUser)
-      const qboProfile = await fetch(
-        "https://accounts.platform.intuit.com/v1/openid_connect/userinfo",
-        {
-          headers: {
-            Authentication: `Bearer ${account?.access_token || token?.accessToken || ""}`,
-            Accept: "application/json",
-          },
-        }
-      )
-
-      console.log("account.access_token: ", account?.access_token)
-      console.log("account ", account)
-      console.log("token.accessToken: ", token.accessToken)
-      console.log("qboProfile: ", qboProfile)
-
-      if (user) {
-        token.id = user.id
-      }
-      if (account) {
-        token.accessToken = account.access_token
-      }
-      return token
-    },
+    jwt: jwtCallback,
   },
 }
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions as any)
