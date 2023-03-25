b06f69be7da18cd8fb47b8496b3286b91fc62825
Gus Ryan <gus.ryan163@gmail.com>
2023-03-25 14:34:57 -0700

    add date formatting and combining classes utility functions

--- lib/util.ts
+++ lib/util.ts
@@ -1,1 +1,10 @@
 export const base64Encode = (str: string) => Buffer.from(str).toString("base64")
+
+export const formatDate = (date: Date) =>
+  `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+
+export function multipleClasses(...args: (string | undefined)[]): string {
+  return args.reduce((prev, curr): string | undefined => {
+    return curr === undefined || curr === "" ? prev : `${prev} ${curr}`
+  }) as string
+}
