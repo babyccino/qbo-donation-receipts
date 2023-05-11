3577d56ac942fd8d177be8f9cec96634ffbf64c7
Gus <gus.ryan163@gmail.com>
2023-05-11 14:19:23 -0700

    add firebase storage

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,13 +4,36 @@ import { DoneeInfo } from "@/components/receipt"
 
 // set env variable FIRESTORE_EMULATOR_HOST to use firebase emulator
 
+const {
+  FIREBASE_PROJECT_ID,
+  FIREBASE_CLIENT_EMAIL,
+  FIREBASE_PRIVATE_KEY,
+  FIREBASE_API_KEY,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+  FIREBASE_STORAGE_EMULATOR_HOST,
+  FIREBASE_STORAGE_EMULATOR_PORT,
+} = process.env
+
+// Firebase config for web api (not needed for firebase-admin)
+// const firebaseConfig = {
+//   apiKey: FIREBASE_API_KEY,
+//   authDomain: `${FIREBASE_PROJECT_ID}.firebaseapp.com`,
+//   projectId: FIREBASE_PROJECT_ID,
+//   storageBucket: `${FIREBASE_PROJECT_ID}.appspot.com`,
+//   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+//   appId: FIREBASE_APP_ID,
+//   measurementId: FIREBASE_MEASUREMENT_ID,
+// }
+
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
       credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        projectId: FIREBASE_PROJECT_ID,
+        clientEmail: FIREBASE_CLIENT_EMAIL,
+        privateKey: FIREBASE_PRIVATE_KEY,
       }),
     })
   } catch (error) {
@@ -39,3 +62,4 @@ const converter = {
 }
 
 export const user = firestore.collection("user").withConverter(converter)
+export const storageBucket = admin.storage().bucket(`${FIREBASE_PROJECT_ID}.appspot.com`)
