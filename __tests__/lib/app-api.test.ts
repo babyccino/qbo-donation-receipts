f76ec9678a63fb86373514f457550fe64cb69655
Gus <gus.ryan163@gmail.com>
2023-05-17 13:55:13 -0700

    fix failing test - fix failing test - add some tests for postJsonData

--- __tests__/lib/app-api.test.ts
+++ __tests__/lib/app-api.test.ts
@@ -1,4 +1,5 @@
 import {
+  postJsonData,
   alreadyFilledIn,
   base64EncodeString,
   fetchJsonData,
@@ -47,6 +48,26 @@ describe("fetchJsonData", () => {
     expect(result).toEqual(expectedData)
   })
 
+  it("fetches data successfully and returns it without access token", async () => {
+    const url = "https://example.com/data"
+    const expectedData = { foo: "bar" }
+
+    mockFetch.mockResolvedValueOnce({
+      ok: true,
+      json: jest.fn().mockResolvedValueOnce(expectedData),
+    } as unknown as Response)
+
+    const result = await fetchJsonData(url)
+
+    expect(mockFetch).toHaveBeenCalledTimes(1)
+    expect(mockFetch).toHaveBeenCalledWith(url, {
+      headers: {
+        Accept: "application/json",
+      },
+    })
+    expect(result).toEqual(expectedData)
+  })
+
   it("throws an error when the response is not ok", async () => {
     const url = "https://example.com/data"
     const accessToken = "someAccessToken"
@@ -57,10 +78,57 @@ describe("fetchJsonData", () => {
       json: jest.fn().mockResolvedValueOnce(errorData),
     } as unknown as Response)
 
-    await expect(fetchJsonData(url, accessToken)).rejects.toEqual({
-      ...errorData,
-      url,
+    await expect(fetchJsonData(url, accessToken)).rejects.toThrow()
+  })
+})
+
+describe("fetchJsonData", () => {
+  let mockFetch: jest.MockedFunction<typeof fetch>
+
+  beforeEach(() => {
+    mockFetch = jest.fn()
+    global.fetch = mockFetch
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it("posts data successfully and returns it", async () => {
+    const url = "https://example.com/data"
+    const postData = { foo: "foo" }
+    const response = { bar: "bar" }
+
+    mockFetch.mockResolvedValueOnce({
+      ok: true,
+      json: jest.fn().mockResolvedValueOnce(response),
+    } as unknown as Response)
+
+    const result = await postJsonData(url, postData)
+
+    expect(mockFetch).toHaveBeenCalledTimes(1)
+    expect(mockFetch).toHaveBeenCalledWith(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postData),
     })
+    expect(result).toEqual(response)
+  })
+
+  it("throws an error when the response is not ok", async () => {
+    const url = "https://example.com/data"
+    const postData = { foo: "foo" }
+    const errorData = { message: "Unauthorized" }
+
+    mockFetch.mockResolvedValueOnce({
+      ok: false,
+      json: jest.fn().mockResolvedValueOnce(errorData),
+    } as unknown as Response)
+
+    await expect(postJsonData(url, postData)).rejects.toThrow()
   })
 })
 
