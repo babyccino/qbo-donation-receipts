ed98c5c86ba3e05a7bba87ce8cbcf997bf92006a
Gus <gus.ryan163@gmail.com>
2023-05-18 13:09:56 -0700

    add get started button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,11 @@ import { GetServerSideProps } from "next"
 import { getServerSession, Session } from "next-auth"
 
 import { authOptions } from "./api/auth/[...nextauth]"
-import { Svg } from "@/components/ui"
+import { buttonStyling, Svg } from "@/components/ui"
 import { multipleClasses } from "@/lib/util"
 import { user } from "@/lib/db"
 import { alreadyFilledIn } from "@/lib/app-api"
+import { Button } from "flowbite-react"
 
 const Card = ({
   title,
@@ -15,12 +16,14 @@ const Card = ({
   href,
   className,
   completed,
+  children,
 }: {
   title: string
   body?: string
   href: string
   className?: string
   completed?: boolean
+  children?: ReactNode
 }) => (
   <Link
     href={href}
@@ -36,6 +39,7 @@ const Card = ({
         <Svg.Tick />
       </div>
     )}
+    {children}
   </Link>
 )
 
@@ -43,48 +47,59 @@ type Props =
   | { session: null; filledIn: false }
   | { session: Session; filledIn: { items: boolean; doneeDetails: boolean } }
 
-export default function IndexPage({ filledIn }: Props) {
-  return (
-    <section className="py-8 px-4 space-y-12 mx-auto max-w-screen-xl text-center lg:py-16">
-      <div>
-        <h1 className="mb-4 text-2xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">
-          Speed up your organisation{"'"}s year-end
-        </h1>
-        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
-          In just a few easy steps we can create and send your client{"'"}s donation receipts
-        </p>
-      </div>
-      <div className="flex flex-col items-center w-full">
-        <Card
-          href="/services"
-          title="Select your qualifying items"
-          body="Select which of your quickbooks sales constitute a gift"
-          completed={filledIn && filledIn.items}
-        />
-        <div className="h-10 w-10 text-slate-400 rotate-180 mt-3">
-          <Svg.HandDrawnUpArrow />
+const IndexPage = ({ filledIn }: Props) => (
+  <section className="py-8 px-4 space-y-12 mx-auto max-w-screen-xl text-center lg:py-16">
+    <div>
+      <h1 className="mb-4 text-2xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">
+        Speed up your organisation{"'"}s year-end
+      </h1>
+      <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+        In just a few easy steps we can create and send your client{"'"}s donation receipts
+      </p>
+      {/* {(!filledIn || (!filledIn.items && !filledIn.doneeDetails)) && ( */}
+      <Link href="/services" className={"text-lg py-3 px-5 " + buttonStyling}>
+        Get started
+        <div className="ml-2 -mb-1 w-5 h-5 inline-block">
+          <Svg.RightArrow />
         </div>
-        <Card
-          href="/details"
-          className="mt-4"
-          title="Enter your organisation's details"
-          body="Enter necessary information such as registration number, signature, company logo, etc."
-          completed={filledIn && filledIn.doneeDetails}
-        />
-        <div className="h-10 w-10 text-slate-400 rotate-180 mt-3">
-          <Svg.HandDrawnUpArrow />
-        </div>
-        <Card
-          href="/generate-receipts"
-          className="mt-4"
-          title="Generate your clients' receipts"
-          body="Receipts can be downloaded individually or all together. We can also automatically email receipts to all qualifying donors"
-          completed={filledIn && filledIn.doneeDetails && filledIn.items}
-        />
+      </Link>
+      {/* )} */}
+    </div>
+    <div className="flex flex-col items-center w-full">
+      <Card
+        href="/services"
+        title="Select your qualifying items"
+        body="Select which of your quickbooks sales constitute a gift"
+        completed={filledIn && filledIn.items}
+      />
+      <div className="h-10 w-10 text-slate-400 rotate-180 mt-3">
+        <Svg.HandDrawnUpArrow />
       </div>
-    </section>
-  )
-}
+      <Card
+        href="/details"
+        className="mt-4"
+        title="Enter your organisation's details"
+        body="Enter necessary information such as registration number, signature, company logo, etc."
+        completed={filledIn && filledIn.doneeDetails}
+      />
+      <div className="h-10 w-10 text-slate-400 rotate-180 mt-3">
+        <Svg.HandDrawnUpArrow />
+      </div>
+      <Card
+        href="/generate-receipts"
+        className="mt-4"
+        title="Generate your clients' receipts"
+        body="Receipts can be downloaded individually or all together. We can also automatically email receipts to all qualifying donors"
+        completed={filledIn && filledIn.doneeDetails && filledIn.items}
+      >
+        {filledIn && filledIn.doneeDetails && filledIn.items && (
+          <p className="font-bold mt-4 text-green-400">We{"'"}re ready to create your receipts!</p>
+        )}
+      </Card>
+    </div>
+  </section>
+)
+export default IndexPage
 
 // --- server-side props ---
 
