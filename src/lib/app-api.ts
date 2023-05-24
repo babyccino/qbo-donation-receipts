cee4629f7facbd1e6fd80585c3d668f40f8ef999
Gus <gus.ryan163@gmail.com>
2023-05-24 15:44:45 -0700

    refactor api handlers

--- src/lib/app-api.ts
+++ src/lib/app-api.ts
@@ -1,5 +1,9 @@
 import { TypeOf, ZodObject, ZodRawShape, z } from "zod"
 import { DbUser } from "./db"
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next"
+import { Session, getServerSession } from "next-auth"
+import { authOptions } from "src/pages/api/auth/[...nextauth]"
+import { ApiError } from "next/dist/server/api-utils"
 
 export async function fetchJsonData<T = any>(url: string, accessToken?: string): Promise<T> {
   const response = await (accessToken
@@ -98,8 +102,35 @@ export function parseRequestBody<T extends ZodRawShape>(shape: T, body: any): Ty
   if (!response.success) {
     const { errors } = response.error
 
-    throw { message: "Invalid request", errors }
+    throw new ApiError(400, JSON.stringify(errors))
   }
 
   return response.data
 }
+
+export type AuthorisedHanlder = (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  session: Session
+) => Promise<void>
+
+export const createAuthorisedHandler =
+  (handler: AuthorisedHanlder, methods: string[]): NextApiHandler =>
+  async (req, res) => {
+    console.log("handler")
+    if (!req.method || !methods.includes(req.method)) {
+      return res.status(405).end()
+    }
+
+    const session = await getServerSession(req, res, authOptions)
+    if (!session) return res.status(401).end()
+
+    try {
+      handler(req, res, session)
+    } catch (error) {
+      console.error(error)
+      if (!(error instanceof ApiError)) return res.status(404).json({ message: "server error" })
+
+      res.status(error.statusCode).json(error)
+    }
+  }
