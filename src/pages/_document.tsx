50cb8916673e5b22eb18453e301e35677b9645d4
Gus <gus.ryan163@gmail.com>
2023-05-24 15:57:37 -0700

    add logo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,12 @@ export default class MyDocument extends Document {
     const pageProps = this.props?.__NEXT_DATA__?.props?.pageProps
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+        </Head>
         <body className="dark:bg-gray-900">
           <Main />
           <NextScript />
