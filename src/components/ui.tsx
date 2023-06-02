e2bb5597fd5a6f32455e0c06f80487017b902666
Gus <gus.ryan163@gmail.com>
2023-06-02 11:52:02 -0700

    add/rename svg elements

--- src/components/ui.tsx
+++ src/components/ui.tsx
@@ -313,7 +313,7 @@ export namespace Svg {
       </g>
     </svg>
   )
-  export const Cross = () => (
+  export const CircledPlus = () => (
     <svg
       fill="none"
       stroke="currentColor"
@@ -326,6 +326,18 @@ export namespace Svg {
     </svg>
   )
   export const Tick = () => (
+    <svg
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
+    </svg>
+  )
+  export const CircledTick = () => (
     <svg
       fill="none"
       stroke="currentColor"
@@ -401,6 +413,55 @@ export namespace Svg {
           y2="14.2"
         />
       </g>
+    </svg>
+  )
+  export const Cross = () => (
+    <svg
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+    </svg>
+  )
+  export const Briefcase = () => (
+    <svg
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
+      ></path>
+    </svg>
+  )
+  export const MapPin = () => (
+    <svg
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+      ></path>
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+      ></path>
     </svg>
   )
 }
--- src/pages/generate-receipts.tsx
+++ src/pages/generate-receipts.tsx
@@ -109,7 +109,7 @@ function ShowReceipt({ Receipt }: { Receipt: () => JSX.Element }) {
           className="fixed right-4 top-4 h-14 w-14 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 z-40"
           onClick={() => setShow(false)}
         >
-          <Svg.Cross />
+          <Svg.CircledPlus />
         </button>
       </div>
     </>
