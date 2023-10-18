ad031b545992eeca2333296ea503583ede1f3b05
Gus <gus.ryan163@gmail.com>
2023-10-17 18:10:18 -0700

    add "use client"

---
+++ src/components/form-client.tsx
@@ -0,0 +1,52 @@
+"use-client"
+
+import { FileInput, Label } from "flowbite-react"
+import { ChangeEventHandler, ReactNode, useState } from "react"
+
+import { supportedExtensions } from "@/lib/util/image-helper"
+
+export function ImageInput({
+  label,
+  id,
+  maxSize,
+  helper,
+  required,
+}: {
+  label: string
+  id: string
+  maxSize: number
+  helper?: ReactNode
+  required?: boolean
+}) {
+  const [error, setError] = useState(false)
+
+  const handleFileInput: ChangeEventHandler<HTMLInputElement> = event => {
+    event.preventDefault()
+    const files = event.target.files
+    if (!files || files.length === 0) return
+    const file = files[0]
+    const extension = file.name.split(".").pop()
+    if (!extension || !supportedExtensions.includes(extension) || file.size > maxSize) {
+      event.target.value = ""
+      return setError(true)
+    }
+
+    return setError(false)
+  }
+
+  return (
+    <p>
+      <Label className="mb-2 inline-block" htmlFor={id}>
+        {label}
+      </Label>
+      <FileInput
+        id={id}
+        name={id}
+        required={required}
+        onChange={handleFileInput}
+        color={error ? "failure" : undefined}
+        helperText={helper}
+      />
+    </p>
+  )
+}
