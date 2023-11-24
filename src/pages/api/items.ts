460ca615b3284f84da8b106f8cce576e52e897cf
Gus <gus.ryan163@gmail.com>
2023-11-24 15:49:35 -0800

    migrate items api to drizzle

--- src/pages/api/items.ts
+++ src/pages/api/items.ts
@@ -1,11 +1,13 @@
 import { z } from "zod"
+import { createId } from "@paralleldrive/cuid2"
 
-import { user } from "@/lib/db"
+import { db } from "@/lib/db/test"
 import {
   AuthorisedHandler,
   createAuthorisedHandler,
   parseRequestBody,
 } from "@/lib/util/request-server"
+import { userDatas } from "db/schema"
 
 export const parser = z.object({
   items: z.array(z.number()),
@@ -13,6 +15,7 @@ export const parser = z.object({
     startDate: z.coerce.date(),
     endDate: z.coerce.date(),
   }),
+  realmId: z.number().int(),
 })
 
 export type DataType = z.infer<typeof parser>
@@ -21,7 +24,31 @@ const handler: AuthorisedHandler = async (req, res, session) => {
   const id = session.user.id
 
   const data = parseRequestBody(parser, req.body)
-  await user.doc(id).set(data, { merge: true })
+  const {
+    items: itemsStr,
+    dateRange: { startDate, endDate },
+    realmId,
+  } = data
+  const items = itemsStr.join(",")
+  await db
+    .insert(userDatas)
+    .values({
+      id: createId(),
+      userId: id,
+      realmId,
+      endDate,
+      startDate,
+      items,
+    })
+    .onConflictDoUpdate({
+      target: [userDatas.userId, userDatas.realmId],
+      set: { startDate, endDate, items },
+    })
+  // const set = await db
+  //   .select()
+  //   .from(userDatas)
+  //   .where(and(eq(userDatas.userId, id), eq(userDatas.realmId, realmId)))
+  // console.log(set)
 
   res.status(200).json({ ok: true })
 }
