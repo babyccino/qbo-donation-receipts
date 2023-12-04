b2a940d49cd74f2ec0da7292c21534f684949f79
Gus <gus.ryan163@gmail.com>
2023-12-03 17:39:27 -0800

    feat(switch companies): add layout data to static routes

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,4 +1,13 @@
+import { and, eq, isNotNull } from "drizzle-orm"
+import { GetServerSideProps } from "next"
+import { getServerSession } from "next-auth"
+import { ApiError } from "next/dist/server/api-utils"
 import Image from "next/image"
+
+import { LayoutProps } from "@/components/layout"
+import { db } from "@/lib/db/test"
+import { accounts, sessions } from "db/schema"
+import { authOptions } from "./api/auth/[...nextauth]"
 
 const Info = () => (
   <section className="flex min-h-screen flex-col p-4 sm:flex-row sm:justify-center">
@@ -37,3 +46,40 @@ const Info = () => (
   </section>
 )
 export default Info
+
+// --- server-side props ---
+
+export const getServerSideProps: GetServerSideProps<LayoutProps> = async ({ req, res }) => {
+  const session = await getServerSession(req, res, authOptions)
+  if (!session) return { props: { session: null } satisfies LayoutProps }
+
+  const accountList = (await db.query.accounts.findMany({
+    columns: { companyName: true, id: true },
+    where: and(isNotNull(accounts.companyName), eq(accounts.userId, session.user.id)),
+  })) as { companyName: string; id: string }[]
+
+  if (session.accountId !== null && accountList.length === 0)
+    throw new ApiError(500, "session has account id but this was not found in the database")
+  if (session.accountId === null && accountList.length > 0) {
+    await db
+      .update(sessions)
+      .set({ accountId: accountList[0].id })
+      .where(eq(sessions.userId, session.user.id))
+    session.accountId = accountList[0].id
+  }
+
+  if (accountList.length > 0)
+    return {
+      props: {
+        session,
+        companies: accountList,
+        selectedAccountId: session.accountId as string,
+      } satisfies LayoutProps,
+    }
+  else
+    return {
+      props: {
+        session,
+      } satisfies LayoutProps,
+    }
+}
--- src/pages/terms/country.tsx
+++ src/pages/terms/country.tsx
@@ -11,3 +11,50 @@ const Country = () => (
   </section>
 )
 export default Country
+
+// --- server-side props ---
+
+import { and, eq, isNotNull } from "drizzle-orm"
+import { GetServerSideProps } from "next"
+import { getServerSession } from "next-auth"
+import { ApiError } from "next/dist/server/api-utils"
+
+import { LayoutProps } from "@/components/layout"
+import { db } from "@/lib/db/test"
+import { accounts, sessions } from "db/schema"
+import { authOptions } from "../api/auth/[...nextauth]"
+
+export const getServerSideProps: GetServerSideProps<LayoutProps> = async ({ req, res }) => {
+  const session = await getServerSession(req, res, authOptions)
+  if (!session) return { props: { session: null } satisfies LayoutProps }
+
+  const accountList = (await db.query.accounts.findMany({
+    columns: { companyName: true, id: true },
+    where: and(isNotNull(accounts.companyName), eq(accounts.userId, session.user.id)),
+  })) as { companyName: string; id: string }[]
+
+  if (session.accountId !== null && accountList.length === 0)
+    throw new ApiError(500, "session has account id but this was not found in the database")
+  if (session.accountId === null && accountList.length > 0) {
+    await db
+      .update(sessions)
+      .set({ accountId: accountList[0].id })
+      .where(eq(sessions.userId, session.user.id))
+    session.accountId = accountList[0].id
+  }
+
+  if (accountList.length > 0)
+    return {
+      props: {
+        session,
+        companies: accountList,
+        selectedAccountId: session.accountId as string,
+      } satisfies LayoutProps,
+    }
+  else
+    return {
+      props: {
+        session,
+      } satisfies LayoutProps,
+    }
+}
