6a64ff497a11afc1b7496bc275b4bfc8e4e006d0
Gus <gus.ryan163@gmail.com>
2023-12-06 01:57:12 -0800

    refactor: simplify types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -51,78 +51,71 @@ const Note = ({ children }: { children?: ReactNode }) => (
 const Card = Object.assign(_Card, { Body, Title, Tick, Note })
 const Arrow = () => <HandDrawnUpArrow className="mt-3 h-10 w-10 rotate-180 text-slate-400" />
 
-type Props = (
-  | { session: Session | null; filledIn: false }
-  | {
-      session: Session
-      filledIn: { items: boolean; doneeDetails: boolean }
-      qboPermission: QboPermission
-    }
-) &
-  LayoutProps
+type Props = {
+  session: Session | null
+  filledIn: { items: boolean; doneeDetails: boolean } | null
+} & LayoutProps
 
-export default function IndexPage(props: Props) {
-  const { filledIn } = props
-  return (
-    <section className="mx-auto max-w-screen-xl space-y-12 p-4 px-4 text-center sm:py-8 lg:py-16">
-      <div>
-        <h1 className="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-3xl lg:text-4xl">
-          Speed up your organisation{"'"}s year-end
-        </h1>
-        <p className="mb-8 text-lg font-normal text-gray-500 dark:text-gray-400 sm:px-16 lg:px-48 lg:text-xl">
-          In just a few easy steps we can create and send your client{"'"}s donation receipts
-        </p>
-        <Show when={!filledIn || (!filledIn.items && !filledIn.doneeDetails)}>
-          <StyledLink href="/items" className="px-5 py-3 text-lg">
-            Get started
-            <ArrowRightIcon className="-mt-1 ml-2 inline-block h-5 w-5" />
-          </StyledLink>
+const IndexPage = ({ filledIn, session }: Props) => (
+  <section className="mx-auto max-w-screen-xl space-y-12 p-4 px-4 text-center sm:py-8 lg:py-16">
+    <div>
+      <h1 className="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-3xl lg:text-4xl">
+        Speed up your organisation{"'"}s year-end
+      </h1>
+      <p className="mb-8 text-lg font-normal text-gray-500 dark:text-gray-400 sm:px-16 lg:px-48 lg:text-xl">
+        In just a few easy steps we can create and send your client{"'"}s donation receipts
+      </p>
+      <Show when={!filledIn || (!filledIn.items && !filledIn.doneeDetails)}>
+        <StyledLink href="/items" className="px-5 py-3 text-lg">
+          Get started
+          <ArrowRightIcon className="-mt-1 ml-2 inline-block h-5 w-5" />
+        </StyledLink>
+      </Show>
+    </div>
+    <div className="flex w-full flex-col items-center">
+      <Card href={filledIn ? "/account" : "/api/auth/signin"}>
+        <Card.Title>Link your account</Card.Title>
+        <Card.Body>
+          Sign in with your QuickBooks Online account and authorise our application
+        </Card.Body>
+        {filledIn && session?.accountId !== undefined && <Card.Tick />}
+      </Card>
+      <Arrow />
+      <Card href="/items" className="mt-4">
+        <Card.Title>Select your qualifying items</Card.Title>
+        <Card.Body>Select which of your QuickBooks sales items constitute a gift</Card.Body>
+        {filledIn && filledIn.items && <Card.Tick />}
+      </Card>
+      <Arrow />
+      <Card href="/details" className="mt-4">
+        <Card.Title>Enter your organisation{"'"}s details</Card.Title>
+        <Card.Body>
+          Enter necessary information such as registration number, signature, company logo, etc.
+        </Card.Body>
+        {filledIn && filledIn.doneeDetails && <Card.Tick />}
+      </Card>
+      <Arrow />
+      <Card href="/generate-receipts" className="mt-4">
+        <Card.Title>Generate your clients{"'"} receipts</Card.Title>
+        <Card.Body>Receipts can be downloaded individually or all together</Card.Body>
+        <Show when={Boolean(filledIn && filledIn.doneeDetails && filledIn.items)}>
+          <Card.Tick />
+          <Card.Note>We{"'"}re ready to create your receipts!</Card.Note>
         </Show>
-      </div>
-      <div className="flex w-full flex-col items-center">
-        <Card href={filledIn ? "/account" : "/api/auth/signin"}>
-          <Card.Title>Link your account</Card.Title>
-          <Card.Body>
-            Sign in with your QuickBooks Online account and authorise our application
-          </Card.Body>
-          {filledIn && props?.qboPermission === QboPermission.Accounting && <Card.Tick />}
-        </Card>
-        <Arrow />
-        <Card href="/items" className="mt-4">
-          <Card.Title>Select your qualifying items</Card.Title>
-          <Card.Body>Select which of your QuickBooks sales items constitute a gift</Card.Body>
-          {filledIn && filledIn.items && <Card.Tick />}
-        </Card>
-        <Arrow />
-        <Card href="/details" className="mt-4">
-          <Card.Title>Enter your organisation{"'"}s details</Card.Title>
-          <Card.Body>
-            Enter necessary information such as registration number, signature, company logo, etc.
-          </Card.Body>
-          {filledIn && filledIn.doneeDetails && <Card.Tick />}
-        </Card>
-        <Arrow />
-        <Card href="/generate-receipts" className="mt-4">
-          <Card.Title>Generate your clients{"'"} receipts</Card.Title>
-          <Card.Body>Receipts can be downloaded individually or all together</Card.Body>
-          <Show when={filledIn && filledIn.doneeDetails && filledIn.items}>
-            <Card.Tick />
-            <Card.Note>We{"'"}re ready to create your receipts!</Card.Note>
-          </Show>
-        </Card>
-        <Arrow />
-        <Card href="/email" className="mt-4">
-          <Card.Title>Send your donors their receipts</Card.Title>
-          <Card.Body>Automatically email receipts to all qualifying donors</Card.Body>
-          <Show when={filledIn && filledIn.doneeDetails && filledIn.items}>
-            <Card.Tick />
-            <Card.Note>We{"'"}re ready to send your receipts!</Card.Note>
-          </Show>
-        </Card>
-      </div>
-    </section>
-  )
-}
+      </Card>
+      <Arrow />
+      <Card href="/email" className="mt-4">
+        <Card.Title>Send your donors their receipts</Card.Title>
+        <Card.Body>Automatically email receipts to all qualifying donors</Card.Body>
+        <Show when={Boolean(filledIn && filledIn.doneeDetails && filledIn.items)}>
+          <Card.Tick />
+          <Card.Note>We{"'"}re ready to send your receipts!</Card.Note>
+        </Show>
+      </Card>
+    </div>
+  </section>
+)
+export default IndexPage
 
 // --- server-side props ---
 
@@ -133,7 +126,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({ req, res }
     return {
       props: {
         session: null,
-        filledIn: false,
+        filledIn: null,
       } satisfies Props,
     }
   }
@@ -174,18 +167,15 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({ req, res }
     account = newAccount
   }
 
-  if (!session.accountId || !account) return { props: { session, filledIn: false } satisfies Props }
+  if (!session.accountId || !account) return { props: { session, filledIn: null } satisfies Props }
 
   // const user = await getUserDataOrThrow(session.user.id)
   const filledIn = { items: Boolean(account.userData), doneeDetails: Boolean(account.doneeInfo) }
-  const qboPermission =
-    account.scope === "accounting" ? QboPermission.Accounting : QboPermission.Profile
 
   return {
     props: {
       session,
       filledIn,
-      qboPermission,
       companies: accountList,
       selectedAccountId: session.accountId,
     } satisfies Props,
