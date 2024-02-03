c7b1e1c79aa9decaaf351f9d2833ad26f839b3d9
Gus <gus.ryan163@gmail.com>
2024-02-03 19:09:02 +0000

    test: add items api route test integration test

--- __tests__/integration/api/items.test.ts
+++ __tests__/integration/api/items.test.ts
@@ -1,18 +1,78 @@
-import { test, describe, expect, beforeAll } from "bun:test"
+import { afterAll, beforeAll, describe, expect, test } from "bun:test"
+import { eq } from "drizzle-orm"
+import { NextApiRequest, NextApiResponse } from "next"
 
-import { createUser } from "../setup"
-import itemsHandler from "@/pages/api/items"
+import { db } from "@/lib/db"
+import { createDateRange } from "@/lib/util/date"
+import handler, { DataType } from "@/pages/api/items"
+import { userDatas } from "db/schema"
+import { createUser, getMockApiContext } from "../mocks"
 
-describe("items", () => {
-  test("should send email", async () => {
-    const { account, deleteAll, session, user } = await createUser()
-    const res = await fetch(makeQueryUrl("123456789", "select * from Item"))
-    const data = await res.text()
-    expect(data).toEqual("John")
+describe("items api route", () => {
+  afterAll(async () => {
+    await db.delete(userDatas)
   })
 
-  test("should create user", async () => {
-    const user = await createUser()
-    expect(user.account.provider).toEqual("QBO")
+  beforeAll(async () => {
+    await db.delete(userDatas)
+  })
+
+  test("sets all fields of test user", async () => {
+    const { account, session, deleteUser } = await createUser(true)
+
+    const items = ["1", "2", "3"]
+    const body: DataType = {
+      dateRange: createDateRange("01-01-2022", "12-31-2022"),
+      items,
+    }
+    const { req, res } = getMockApiContext("POST", session.sessionToken, {
+      ...body,
+      dateRange: {
+        startDate: body.dateRange.startDate.toISOString(),
+        endDate: body.dateRange.endDate.toISOString(),
+      },
+    })
+    const response = await handler(
+      req as unknown as NextApiRequest,
+      res as unknown as NextApiResponse,
+    )
+
+    // expect(res.json).toHaveBeenCalledWith({ ok: true })
+
+    const userData = await db.query.userDatas.findFirst({
+      where: eq(userDatas.accountId, account.id),
+    })
+
+    expect(userData).toBeDefined()
+    expect(userData?.items).toBe(items.join(","))
+    expect(userData?.startDate?.toISOString()).toBe(body.dateRange.startDate.toISOString())
+    expect(userData?.endDate?.toISOString()).toBe(body.dateRange.endDate.toISOString())
+
+    await deleteUser()
+  })
+
+  test("disconnected user returns 401", async () => {
+    const { user, account, session, deleteUser } = await createUser(false)
+
+    const items = ["1", "2", "3"]
+    const body: DataType = {
+      dateRange: createDateRange("01-01-2022", "12-31-2022"),
+      items,
+    }
+    const { req, res } = getMockApiContext("POST", session.sessionToken, {
+      ...body,
+      dateRange: {
+        startDate: body.dateRange.startDate.toISOString(),
+        endDate: body.dateRange.endDate.toISOString(),
+      },
+    })
+    const response = await handler(
+      req as unknown as NextApiRequest,
+      res as unknown as NextApiResponse,
+    )
+
+    // expect(res.status).toHaveBeenCalledWith(401)
+
+    await deleteUser()
   })
 })
